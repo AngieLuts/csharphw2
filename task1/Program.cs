82f23946016782b2b093c575f57de7a6498171e3
AngieLuts <angiegk@yandex.ru>
2023-10-08 14:31:44 +0300

    t1 com 1

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+Random rand = new Random();
+int initNumber = rand.Next(100, 1000);
+int hnum= initNumber/10;
+int result = hnum%10;
+System.Console.WriteLine($"{initNumber} ==> {result}");
